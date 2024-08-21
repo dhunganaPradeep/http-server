19f3ddbd404aea8a7741647641e3ff14dae0911d
dhunganaPradeep <pradeep.dhungana23@gmail.com>
2024-08-21 12:24:08 +0545

    test

--- src/Server.cs
+++ src/Server.cs
@@ -9,13 +9,39 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 
-var socket = server.AcceptSocket();
+while (true)
+{
+    using Socket socket = server.AcceptSocket();
+    
+    // Buffer to store the incoming request
+    byte[] buffer = new byte[1024];
+    int received = socket.Receive(buffer);
+    
+    // Convert the bytes received into a string
+    string request = Encoding.UTF8.GetString(buffer, 0, received);
+    
+    // Extract the request line (first line of the request)
+    string requestLine = request.Split("\r\n")[0];
+    
+    // Extract the URL path from the request line
+    string[] requestParts = requestLine.Split(' ');
+    string method = requestParts[0];
+    string urlPath = requestParts[1];
 
-// Construct the HTTP response
-string httpResponse = "HTTP/1.1 200 OK\r\n" +
-                      "Content-Length: 0\r\n" +
-                      "Connection: close\r\n" +
-                      "\r\n";
-socket.Send(Encoding.UTF8.GetBytes(httpResponse));
-socket.Close();
+    // Determine the response based on the URL path
+    string httpResponse;
+
+    if (urlPath == "/")
+    {
+        httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+    }
+    else
+    {
+        httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+    }
+
+    // Send the response
+    socket.Send(Encoding.UTF8.GetBytes(httpResponse));
+}
+
 server.Stop();
